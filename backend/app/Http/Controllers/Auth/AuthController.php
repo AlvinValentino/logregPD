424aed19e5a612013ac1fda1fefa96e06c2cb8b6
AlvinValentino <alvinvalentino986@gmail.com>
2022-08-13 10:46:46 +0700

    initial commit

---
+++ backend/app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller {
+    
+}
